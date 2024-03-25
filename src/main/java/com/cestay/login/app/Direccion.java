d5130ce0b69f5cfde0efb5174871850f6e61ed71
Christian Estay <cestay@atomix.cl>
2024-03-25 14:52:43 -0300

    v1

--- src/main/java/com/cestay/login/app/Direccion.java
+++ src/main/java/com/cestay/login/app/Direccion.java
@@ -2,4 +2,35 @@ package com.cestay.login.app;
 
 public class Direccion {
 
+    private String calle;
+    private String numero;
+    private String ciudad;
+    private String comuna;
+
+    public Direccion() {
+    }
+
+    public Direccion(String calle, String numero, String ciudad, String comuna) {
+        this.calle = calle;
+        this.numero = numero;
+        this.ciudad = ciudad;
+        this.comuna = comuna;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public String getComuna() {
+        return comuna;
+    }
+
 }
--- src/main/java/com/cestay/login/app/Usuario.java
+++ src/main/java/com/cestay/login/app/Usuario.java
@@ -1,5 +1,57 @@
 package com.cestay.login.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Usuario {
+
+    private long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private long perfil;
+    private List<Direccion> direcciones;
+
+    public Usuario() {
+    }
+
+    public Usuario(long id, String nombre, String apellido, String email, String password, long perfil) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.password = password;
+        this.perfil = perfil;
+        this.direcciones = new ArrayList<>();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public long getPerfil() {
+        return perfil;
+    }
+
+    public void setDireccion(Direccion direccion) {
+        this.direcciones.add(direccion);
+    }
 
 }
